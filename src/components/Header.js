53c2da1668d0c301eacecc896e3e9b33b7e6018d
thomasIRA <thomas.al@live.ca>
2024-01-03 11:41:14 -0500

    init and header

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+
+const Header = (props) => {
+    return (
+        <header className="container bg-white p-2">
+            <h1 className="">{ props.title }</h1>
+        </header>
+    );
+}
+
+export default Header;
