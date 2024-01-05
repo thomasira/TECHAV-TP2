61b3c94b495cf7679b107b9f4272e31a60b4c2e0
thomasIRA <thomas.al@live.ca>
2024-01-05 10:27:53 -0500

    change logo color

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-import { useEffect, useState } from "react";
-import Products from "./components/Products";
-import { Link } from "react-router-dom";
-
+import { useEffect, useState } from 'react';
+import Products from './components/Products';
+import { Link } from 'react-router-dom';
 import {BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from "./components/Home";
+import Home from './components/Home';
+import BbLogo from './components/BbLogo';
 
 function App() {
     const [products, setProducts] = useState([
         {
-        "id": 1,
-        "name": "Yogurt",
-        "description": "greek yogurt",
-        "price": 45.34,
-        "category": "food"
+        'id': 1,
+        'name': 'Yogurt',
+        'description': 'greek yogurt',
+        'price': 45.34,
+        'category': 'food'
         },
         {
-        "id": 2,
-        "name": "Sofa",
-        "description": "comfy sofa",
-        "price": 2032.99,
-        "category": "furniture"
+        'id': 2,
+        'name': 'Sofa',
+        'description': 'comfy sofa',
+        'price': 2032.99,
+        'category': 'furniture'
         },
         {
-        "id": 3,
-        "name": "The big atlas",
-        "description": "an atlas of the world",
-        "price": 43.29,
-        "category": "book"
+        'id': 3,
+        'name': 'The big atlas',
+        'description': 'an atlas of the world',
+        'price': 43.29,
+        'category': 'book'
         },
         {
-        "id": 8,
-        "name": "Wok",
-        "description": "cast iron wok",
-        "price": 67.99,
-        "category": "kitchen tool"
+        'id': 8,
+        'name': 'Wok',
+        'description': 'cast iron wok',
+        'price': 67.99,
+        'category': 'kitchen tool'
         }
-    ])
+    ]);
     const addProduct = product => { 
         product.id = products[products.length - 1].id + 1;
         setProducts([...products, product]);
@@ -57,18 +57,19 @@ function App() {
 
     return (
         <BrowserRouter>
-        <div className="bg-light text-dark font-archivo h-screen p-3 flex flex-col items-center gap-5">
+        <BbLogo/>
+        <div className='bg-light text-dark text-text font-archivo h-screen p-3 flex flex-col items-center gap-5'>
             <nav>
-                <Link to="/">Home</Link>
-                <Link to="/products">Products</Link>
+                <Link to='/'>Home</Link>
+                <Link to='/products'>Products</Link>
             </nav>
             
             <Routes>
                 <Route path='/' element={ <Home/> }/>
                 <Route path='/products' element={ products.length > 0 ? (
                     <Products products={ products } onDelete={ deleteProduct } onUpdate={ updateProduct } onAdd={ addProduct }/>
-                    ) : (
-                        <h2 className="text-light font-bold p-3 text-center text-red-500">No product available</h2>
+                ) : (
+                    <h2 className='text-light font-bold p-3 text-center text-red-500'>No product available</h2>
                 )}/>
             </Routes>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,17 @@ module.exports = {
       },
       fontFamily: {
         archivo:['Archivo, sans-serif']
+      },
+      fontSize: {
+        xs: 'clamp(12px, 1.5vw, 16px)',
+        text: 'clamp(16px, 1.75vw, 22px)',
+        s: 'clamp(20px, 3vw, 28px)',
+        m: 'clamp(24px, 3.75vw, 32px)',
+        l: 'clamp(32px, 5vw, 48px)',
+        xl: 'clamp(42px, 7vw, 60px)',
+        huge: 'clamp(60px, 9.25vw, 96px)',
       }
     },
   },
   plugins: [],
 }
-
